b9f636b4304e0e3a7b1031a912f2348d24a1ad63
Paul-Passeron <paul.passeron2@gmail.com>
2022-12-05 21:20:36 +0100

    Commencé l'écriture de la fonction update qui est le coeur de l'algo.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 #####################################################
 
 import math
+import random
 
 class Cell:
     def __init__(self, i, j):
@@ -18,26 +19,34 @@ class Cell:
         self.prediction = 0
         self.cell_type = 1  # 0: wall, barrier; 1: floor cell, exit
         self.predicted_direction = (0, 0)
+        self.is_blocker_of = []
+        self.bounded_to = None
+        self.next_update = 0
 
     def get_probability(self, d):
         return 0
 
+
 def get_reaction_surrounding():
-        return [[Cell(-1, 1), Cell(0, 1), Cell(1, 1)], [Cell(-1, 0), None, Cell(1, 0)], [Cell(-1, -1), Cell(0, -1), Cell(1, -1)]]
+    return [[Cell(-1, 1), Cell(0, 1), Cell(1, 1)], [Cell(-1, 0), None, Cell(1, 0)], [Cell(-1, -1), Cell(0, -1), Cell(1, -1)]]
+
 
 class room():
 
-    def __init__ (self):
+    def __init__(self):
         self.total_n_of_persons = 10
         self.potential_strength = -3
         self.dimensions = (10, 10)
         self.exit = Cell()
-        self.cells = {(i, j): Cell(i, j) for i in range(self.dimensions[0]) for j in range(self.dimensions[1])}
+        self.cells = {(i, j): Cell(i, j) for i in range(
+            self.dimensions[0]) for j in range(self.dimensions[1])}
         self.N = 1
         self.alpha = 1
         self.beta = 1
         self.gamma = 1
-        
+        self.time_unit = 0.7
+        self.period = 1
+
     def add_cells(self, c1, c2):
         if c1 == None:
             return c2
@@ -49,6 +58,7 @@ class room():
 
     def updateN(self):
         return 1
+
     def get_distance(self, x):
         return math.sqrt(10*(self.exit.i-x.i)**2/(self.exit.j-x.j)+(self.exit.j-x.j)**2)
 
@@ -68,10 +78,46 @@ class room():
         return t * math.exp(self.alpha*u)*(1-self.beta*n)*(1-self.gamma*r_prime)
 
     def get_probabilities(self, x):
-        dic = {(i, j): self.get_unnormalized_probs(x, Cell(i, j)) for i in range(-1, 2) for j in range(-1, 2) if i != j}
+        dic = {(i, j): self.get_unnormalized_probs(x, Cell(i, j))
+               for i in range(-1, 2) for j in range(-1, 2) if (i != j or i == 1)}
         inv_N = 0
         for k in dic:
             inv_N += dic[k]
         for k in dic:
             dic[k] /= inv_N
-        return dic
+        return
+
+    def get_agent_to_update(self):
+        dic = {}
+        for k in self.cells:
+            c = self.cells[k]
+            if c.n == 1:
+                if c.next_update in dic:
+                    dic[c.next_update].append(c)
+                else:
+                    dic[c.next_update] = [c]
+        min_k = float("inf")
+        for k in dic:
+            if k < min_k:
+                min_k = k
+        return dic[min_k]
+
+    def update_cells(self, cell_array):
+        penalty = 1
+        for c in cell_array:
+            probabilities = self.get_probabilities(c)
+            last_prob = 0
+            dic_of_probs = {}
+            for k in probabilities:
+                last_prob+=probabilities[k]
+                dic_of_probs[int(1000*last_prob)] = k
+            rand_n = random.randint(0, 1000)
+            closest_k = None
+            for k in dic_of_probs:
+                if closest_k == None:
+                    closest_k = k
+                elif math.abs(closest_k-rand_n) < math.abs(closest_k-rand_n):
+                    closest_k = k
+            target_dir = dic_of_probs[closest_k]
+            target = self.cells[self.add_cells(c, target_dir)]
+            c.next_update += self.period * penalty
